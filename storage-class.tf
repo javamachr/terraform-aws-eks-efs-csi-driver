8bd368ac718ff7e36eb065e042ec2e1cf798e530
javamachr <jakub.vavrik@gmail.com>
2022-03-09 11:58:18 +0100

    Update storage-class.tf

--- storage-class.tf
+++ storage-class.tf
@@ -4,5 +4,9 @@ resource "kubernetes_storage_class" "storage_class" {
     name = var.storage_class_name
   }
   storage_provisioner = "efs.csi.aws.com"
+  parameters = {
+    provisioningMode = "efs-ap" # Access point
+    directoryPerms   = "700"
+  }
   depends_on          = [helm_release.kubernetes_efs_csi_driver]
 }
