75e92d81e466be50825acde16c314ac940b3e12b
javamachr <jakub.vavrik@gmail.com>
2022-03-09 11:38:49 +0100

    Update versions.tf
    removed upper bound version constraint

--- versions.tf
+++ versions.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 0.13"
 
   required_providers {
-    aws        = ">= 3.13, < 4.0"
+    aws        = ">= 3.13"
     helm       = ">= 1.0, < 3.0"
-    kubernetes = ">= 1.10.0, < 3.0.0"
+    kubernetes = ">= 1.10.0"
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.9.4"
